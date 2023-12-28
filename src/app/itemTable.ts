61171393759aab521e88ccda585f060e447850ac
youngho kim <yhkim@blockodyssey.io>
2023-12-28 13:45:24 +0900

    first commit

---
+++ src/app/itemTable.ts
@@ -0,0 +1,178 @@
+export const ITEM_TABLE = [
+    {
+        name:'일취월장-1일',
+        percentage: 0.09,
+    },
+    {
+        name:'다다익선-1일',
+        percentage: 0.09,
+    },
+    {
+        name:'자아도취-1일',
+        percentage: 0.09,
+    },
+    {
+        name:'축지법-1일',
+        percentage: 0.09,
+    },
+    {
+        name:'소탕령',
+        percentage: 0.085,
+    },
+    {
+        name:'변줌-조철희',
+        percentage: 0.085,
+    },
+    {
+        name:'변줌-이명화',
+        percentage: 0.085,
+    },
+    {
+        name:'꼬망(영랑(火))-1일(거래불가)',
+        percentage: 0.0625,
+    },
+    {
+        name:'꼬망(영랑(雷))-1일(거래불가)',
+        percentage: 0.0625,
+    },
+    {
+        name:'일석이조-5일',
+        percentage: 0.03,
+    },
+    {
+        name:'일취월장-15일(거래불가)',
+        percentage: 0.03,
+    },
+    {
+        name:'신선단약-7일(거래불가)',
+        percentage: 0.03,
+    },
+    {
+        name:'다다익선-7일(거래불가)',
+        percentage: 0.025,
+    },
+    {
+        name:'도발부-7일(거래불가)',
+        percentage: 0.025,
+    },
+    {
+        name:'고급소탕령초기화주문서(거래불가)',
+        percentage: 0.025,
+    },
+    {
+        name:'두신의석상',
+        percentage: 0.012845,
+    },
+    {
+        name:'원공의석상',
+        percentage: 0.012845,
+    },
+    {
+        name:'기의석상',
+        percentage: 0.012845,
+    },
+    {
+        name:'누에의석상',
+        percentage: 0.012845,
+    },
+    {
+        name:'백종',
+        percentage: 0.01,
+    },
+    {
+        name:'불사조',
+        percentage: 0.01,
+    },
+    {
+        name:'초몬꾸',
+        percentage: 0.009,
+    },
+    {
+        name:'중몬꾸',
+        percentage: 0.005,
+    },
+    {
+        name:'지국예복제조서',
+        percentage: 0.0015,
+    },
+    {
+        name:'광목갑옷제조서',
+        percentage: 0.0015,
+    },
+    {
+        name:'백호석상',
+        percentage: 0.001,
+    },
+    {
+        name:'주작석상',
+        percentage: 0.001,
+    },
+    {
+        name:'청룡석상',
+        percentage: 0.001,
+    },
+    {
+        name:'현무석상',
+        percentage: 0.001,
+    },
+    {
+        name:'고몬꾸',
+        percentage: 0.001,
+    },
+    {
+        name:'지국천왕반지',
+        percentage: 0.0001,
+    },
+    {
+        name:'광목천왕지환',
+        percentage: 0.0001,
+    },
+    {
+        name:'금강야차명왕의팔보호구',
+        percentage: 0.00018,
+    },
+    {
+        name:'금강야차명왕의요대',
+        percentage: 0.00014,
+    },
+    {
+        name:'금강야차명왕의신발',
+        percentage: 0.00015,
+    },
+    {
+        name:'군다리명왕의팔보호구',
+        percentage: 0.00018,
+    },
+    {
+        name:'군다리명왕의요대',
+        percentage: 0.00014,
+    },
+    {
+        name:'군다리명왕의신발',
+        percentage: 0.00015,
+    },
+    {
+        name:'금강야차명왕의머리띠',
+        percentage: 0.0001,
+    },
+    {
+        name:'금강야차명왕의갑옷',
+        percentage: 0.00008,
+    },
+    {
+        name:'군다리명왕의머리띠',
+        percentage: 0.0001,
+    },
+    {
+        name:'군다리명왕의갑옷',
+        percentage: 0.00008,
+    },
+    {
+        name:'금강야차가락지',
+        percentage: 0.00006,
+    },
+    {
+        name:'군다리명왕가락지',
+        percentage: 0.00006,
+    },
+]
